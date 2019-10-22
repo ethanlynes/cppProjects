d3c2a746e2314c554d6a3955d9813f886ab49d1f
U-BSD\477933 <477933@SHS-10DECC90A.beaverton.k12.or.us>
2019-10-22 09:45:15 -0700

    thanks my dude

--- StudentList/studentlist.cpp
+++ StudentList/studentlist.cpp
@@ -4,11 +4,16 @@
 using namespace std;
 
 /*
-Ethan Lynes
-Date
-yeet
+ Ethan Lynes
+
+ Date: 10/22/19
+
+ This program resembles a student data system. You are able to type a few commands
+ that will allow you to manipulate the data. There are commands to add students and delete students.
+ There are also commands for listing the students in the system and quitting the program entirely.
 */
 
+// student struct
 struct Student {
   char first_name[20];
   char last_name[20];
@@ -16,6 +21,7 @@ struct Student {
   float gpa;
 };
 
+// init methods
 void addStudent(vector<Student> &students);
 void listStudents(vector<Student> students);
 void delStudent(vector<Student> &students, int id);
@@ -23,24 +29,30 @@ void delStudent(vector<Student> &students, int id);
 int main() {
   bool running = true;
 
+  // vector of students
   vector<Student> students;
 
   char input[5];
+  //loops program until running = false
   while (running == true) {
 
+    // asks user for a command
     cout << "actions: ADD, DEL, LIST, QUIT" << endl;
     cin.get(input,5);
     cin.get();
 
+    // checks which command
     if (input[0] == 'A' &&
 	input[1] == 'D' &&
 	input[2] == 'D') {
-      
+
+      // if ADD, call addstudent method
       addStudent(students);
     } else if (input[0] == 'D' &&
 	       input[1] == 'E' &&
 	       input[2] == 'L') {
 
+      // if DEL, ask for a student id and calls deletestudent method
       int id;
       cout << "Enter the id of the student you want to remove" << endl;
       cin >> id;
@@ -51,23 +63,34 @@ int main() {
 	       input[2] == 'S' &&
 	       input[3] == 'T') {
 
+      // if LIST, call liststudents method
       listStudents(students);
     } else if (input[0] == 'Q' &&
 	       input[1] == 'U' &&
 	       input[2] == 'I' &&
 	       input[3] == 'T') {
 
+      // if QUIT, end while loop, ending the program
       cout << "Terminating..." << endl;
       running = false;
     } else {
+
+      // if user input is invalid, throw error message and quit program
       cout << "Invalid command" << endl;
       running = false;
     }
   }
 }
 
+
+// method for adding students to the vector
 void addStudent(vector<Student> &students) {
+
+  // creates a temporary student struct
   Student temp;
+
+  // asks for user input for a name, id, and gpa
+  // adds each to the student struct
   cout << "First name: ";
   cin.get(temp.first_name,20);
   cin.get();
@@ -83,26 +106,47 @@ void addStudent(vector<Student> &students) {
   cout << "GPA: ";
   cin >> temp.gpa;
   cin.get();
-  
+
+  // adds the data from the temporary student to the vector
   students.push_back(temp);
   cout << endl;
 }
 
+
+// method for listing students to the console
 void listStudents(vector<Student> students) {
+
+  // outputs the vector's size (number of students)
   cout << "Size: " << students.size() << endl;
+
+  // iterates through the vector
   for (int i = 0; i < students.size(); i++) {
+
+    // for each student, output their first name, last name, and id
     cout << students[i].first_name << " " << students[i].last_name << ", ";
     cout << "ID: " << students[i].id << ", ";
+
+    // set the gpa to show 2 decimal places
     cout.precision(3);
     cout.setf(ios::showpoint);
+
+    // output their gpa
     cout << "GPA: " << students[i].gpa << endl;
   }
   cout << endl;
 }
 
+
+// method for deleting students from the vector
 void delStudent(vector<Student> &students, int id) {
+
+  // iterates through the vector
   for (int i = 0; i < students.size(); i++) {
+    
+    // checks if a student's id is equal to the input given
     if (students[i].id == id) {
+      
+      // if so, remove the student's data from the vector
       students.erase(students.begin()+i);
     }
   }
