1bf963819fb8c434c210f68b7e8ddba00d5d0271
U-BSD\477933 <477933@SHS-10DECC90A.beaverton.k12.or.us>
2019-11-21 10:49:34 -0800

    thanks my dude

--- Zuul/main.cpp
+++ Zuul/main.cpp
@@ -6,18 +6,85 @@
 
 using namespace std;
 
+/*
+Name: Ethan Lynes
+Date:
+Program:
+
+*/
+
+//function declarations
 void createRooms();
+void listCommands();
+bool processCommand(char command[10]);
+bool isCommand(char command[10]);
+void goRoom(char command[10]);
 
 char SOUTH[6] = "south";
 char NORTH[6] = "north";
 char EAST[5] = "east";
 char WEST[5] = "west";
 
+char validCommands[][10] = {
+    "go", "quit", "help", "inventory", "get", "drop"
+};
+
 int main() {
   
   createRooms();
+
+  cout << "Type 'help' for a list of commands." << endl;
+  
+  bool finished = false;
+  while (!finished) {
+    
+    char command[10];
+    cin.get(command,10);
+    cin.get();
+    
+    finished = processCommand(command);
+  }
+  
   return 0;
 }
+
+
+void listCommands() {
+  cout << "Commands: ";
+  for (int i = 0; i < 6; i++) {
+    cout << validCommands[i] << ", ";
+  }
+  cout << endl << endl;
+}
+
+
+bool processCommand(char command[10]) {
+
+  bool wantToQuit = false;
+  
+  if (!isCommand(command)) {
+    cout << "That is not a valid command!" << endl;
+    return false;
+  }
+  
+  if (!strcmp(command,"help")) {
+    listCommands();
+  } else if (!strcmp(command,"quit")) {
+    wantToQuit = true;
+  }
+  return wantToQuit;
+}
+
+
+bool isCommand(char command[10]) {
+  for (int i = 0; i < 6; i++) {
+    if (!strcmp(command,validCommands[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
 
 void createRooms() {
 
@@ -43,4 +110,9 @@ void createRooms() {
 
   office->setExit(EAST,lobby);
   lobby->setExit(WEST,office);
+}
+
+
+void goRoom(char command[10]) {
+
 }
