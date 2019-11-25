c22b426f41a3292db5b7a533633a6207c7033096
U-BSD\477933 <477933@SHS-10DECC90A.beaverton.k12.or.us>
2019-11-25 10:49:36 -0800

    thanks my dude

--- Zuul2/main.cpp
+++ Zuul2/main.cpp
@@ -41,17 +41,79 @@ int main() {
   // rooms
   Room* currentRoom;
   
-  char office_d[] = "You are in the office.";
-  char office_n[] = "The office.";
-  Room* office = new Room(office_d,office_n);
+  char entrance_d[] = "You are at the entrance of the supermarket.";
+  char entrance_n[] = "The Entrance.";
+  Room* entrance = new Room(entrance_d,entrance_n);
+
+  char center_d[] = "You are at the center of the supermarket.";
+  char center_n[] = "The center.";
+  Room* center = new Room(center_d,center_n);
   
-  char lobby_d[] = "You are in the lobby.";
-  char lobby_n[] = "The lobby.";
-  Room* lobby = new Room(lobby_d,lobby_n);
+  char checkout_d[] = "You are at checkout.";
+  char checkout_n[] = "checkout";
+  Room* checkout = new Room(checkout_d,checkout_n);
+
+  char starbucks_d[] = "You are in Starbucks.";
+  char starbucks_n[] = "Starbucks.";
+  Room* starbucks = new Room(starbucks_d,starbucks_n);
+
+  char aisle1_d[] = "You are in aisle 1.";
+  char aisle1_n[] = "aisle 1.";
+  Room* aisle1 = new Room(aisle1_d,aisle1_n);
+
+  char aisle2_d[] = "You are in aisle 2.";
+  char aisle2_n[] = "aisle 2.";
+  Room* aisle2 = new Room(aisle2_d,aisle2_n);
+
+  char aisle3_d[] = "You are in aisle 3.";
+  char aisle3_n[] = "aisle 3.";
+  Room* aisle3 = new Room(aisle3_d,aisle3_n);
+
+  char aisle4_d[] = "You are in aisle 4.";
+  char aisle4_n[] = "aisle 4.";
+  Room* aisle4 = new Room(aisle4_d,aisle4_n);
+
+  char aisle5_d[] = "You are in aisle 5.";
+  char aisle5_n[] = "aisle 5.";
+  Room* aisle5 = new Room(aisle5_d,aisle5_n);
+
+  char chips_d[] = "You are in the chip aisle.";
+  char chips_n[] = "chip aisle.";
+  Room* chips = new Room(chips_d,chips_n);
+
+  char bread_d[] = "You are in the bread aisle.";
+  char bread_n[] = "bread aisle.";
+  Room* bread = new Room(bread_d,bread_n);
+
+  char drinks_d[] = "You are in the drinks aisle.";
+  char drinks_n[] = "drinks aisle.";
+  Room* drinks = new Room(drinks_d,drinks_n);
+
+  char condiments_d[] = "You are in the condiment aisle.";
+  char condiments_n[] = "condiments aisle.";
+  Room* condiments = new Room(condiments_d,condiments_n);
+
+  char dairy_d[] = "You are in the dairy aisle.";
+  char dairy_n[] = "dairy aisle.";
+  Room* dairy = new room(dairy_d,dairy_n);
+
+  char seafood_d[] = "You are in the seafood aisle.";
+  char seafood_n[] = "seafood aisle.";
+  Room* seafood = new Room(seafood_d,seafood_n);
+
+  char meat_d[] = "You are in the meat aisle.";
+  char meat_n[] = "meat aisle.";
+  Room* meat = new Room(meat_d,meat_n);
+
+  char meat2_d[] = "You are in the second meat aisle.";
+  char meat2_n[] = "second meat aisle.";
+  Room* meat2 = new Room(meat2_d, meat2_n);
   
+  // set exits
   lobby->setExit(WEST,office);
   office->setExit(EAST,lobby);
   currentRoom = office;
+
   
   // items
   char pencil_n[] = "pencil";
